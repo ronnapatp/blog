12e42e77bbfa5668791987b98fa702e3d90932cb
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-03 10:11:26 +0700

    Update footer and meta

--- components/Footer.js
+++ components/Footer.js
@@ -11,7 +11,6 @@ export default function Footer() {
           <SocialIcon kind="github" href={siteMetadata.github} size="6" />
           <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
           <SocialIcon kind="youtube" href={siteMetadata.youtube} size="6" />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
         </div>
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
@@ -23,7 +22,7 @@ export default function Footer() {
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
+            Generate from Tailwind Nextjs Theme
           </Link>
         </div>
       </div>
