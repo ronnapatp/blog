fa3a8df5ed5ea30a42359f3e8acdf3cf10cd01d8
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-02-08 12:59:38 +0700

    Update navbar

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,6 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
   { href: 'https://ronnapat.com', title: 'Main website' },
 ]
 
