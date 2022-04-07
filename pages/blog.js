01fad63e0b9a906b17dc6aa8e867092f03ab9d4f
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-04-07 09:56:16 +0700

    Add ads

--- pages/blog.js
+++ pages/blog.js
@@ -26,13 +26,18 @@ export default function Blog({ posts, initialDisplayPosts, pagination }) {
         pagination={pagination}
         title="All Posts"
       />
-
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1196910141338252"
+        crossOrigin="anonymous"
+      ></script>
       <ins
-        className="adsbygoogle block"
-        data-ad-format="fluid"
-        data-ad-layout-key="-gw-3+1f-3d+2z"
+        className="adsbygoogle"
+        style="display:block"
         data-ad-client="ca-pub-1196910141338252"
-        data-ad-slot="3634605013"
+        data-ad-slot="8233499034"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
       ></ins>
       <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
     </>
