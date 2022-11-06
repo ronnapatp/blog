4d1c9ee856277b91323b8a0a26327257d492dc4e
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-11-06 21:43:32 +0700

    Add google analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -17,7 +17,7 @@ const siteMetadata = {
   analytics: {
     // supports plausible, simpleAnalytics, umami or googleAnalytics
     plausibleDataDomain: 'blog.ronnapat.com', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
+    simpleAnalytics: true, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: 'G-YFPY7KW0YY', // e.g. UA-000000-2 or G-XXXXXXX
   },
