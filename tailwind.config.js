8c3ff543b132dfd7d695649bc0ae1a786bef4f2f
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2023-02-28 21:22:02 +0700

    Update font and remove some images

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
         14: '3.5rem',
       },
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ['Charis SIL', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: colors.teal,
