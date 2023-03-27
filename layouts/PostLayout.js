268ab8588313fb5517e681519f92e0b1e563377c
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2023-03-27 17:35:56 +0700

    Update layout

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -25,17 +25,14 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
           <header className="pt-6 xl:pb-6">
             <div className="space-y-1 text-center">
-              <dl className="space-y-10">
-                <div>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <span className="text-black dark:text-white font-bold">Published on </span>
-                    <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-                    </time>
-                  </dd>
+              <dl className="space-y-10"></dl>
+              {tags && (
+                <div className="">
+                  {tags.map((tag) => (
+                    <Tag key={tag} text={tag} />
+                  ))}
                 </div>
-              </dl>
+              )}
               <div className="pb-5">
                 <PageTitle>{title}</PageTitle>
               </div>
@@ -78,6 +75,14 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                       </dl>
                     </li>
                   ))}
+                  <div>
+                    <dt className="sr-only">Published on</dt>
+                    <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                      <time dateTime={date}>
+                        {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
+                      </time>
+                    </dd>
+                  </div>
                 </ul>
               </dd>
             </dl>
@@ -119,18 +124,6 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
             </div>
             <footer>
               <div className="text-sm font-medium leading-5 divide-gray-200 xl:divide-y dark:divide-gray-700 xl:col-start-1 xl:row-start-2">
-                {tags && (
-                  <div className="py-4 xl:py-8">
-                    <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
-                      Tags
-                    </h2>
-                    <div className="flex flex-wrap">
-                      {tags.map((tag) => (
-                        <Tag key={tag} text={tag} />
-                      ))}
-                    </div>
-                  </div>
-                )}
                 {(next || prev) && (
                   <div className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
                     {prev && (
